ecb469b7a339b4747812d6d83145e29e394704ea
conjuncts <67614673+conjuncts@users.noreply.github.com>
2025-04-22 14:01:39 -0500

    Jumping Skeleton Mage

--- game-engine-assignment/Scripts/ai/character_wrapper.gd
+++ game-engine-assignment/Scripts/ai/character_wrapper.gd
@@ -1,13 +1,22 @@
 extends CharacterBody3D
 
-var movement_speed: float = 2.0
-var movement_target_position: Vector3 = Vector3(-3.0,0.0,2.0)
+@export var movement_speed: float = 3.0
+@export var jump_impulse = 0.4
 
-var timer: Timer
+# If there is an offset between desired height and a player's real height
+@export var height_offset_fix = 0.406784058
+
 
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 
+@onready var proto_controller: CharacterBody3D = get_node_or_null("../ProtoController") if get_node_or_null("../ProtoController") else get_node_or_null("../../ProtoController")
+
+# detect the mage getting "stuck"
+var last_position
+var real_velocity
+
 func _ready():
+	
 	# These values need to be adjusted for the actor's speed
 	# and the navigation layout.
 	navigation_agent.path_desired_distance = 0.5
@@ -16,17 +25,16 @@ func _ready():
 	# Make sure to not await during _ready.
 	actor_setup.call_deferred()
 	
-	# set target to position of $../ProtoController
-	timer = Timer.new()
+
+	# Remove from any existing groups to avoid duplicates
+	if is_in_group("Enemy"):
+		remove_from_group("Enemy")
 	
-	# Set the timer to trigger every 10 seconds
-	timer.wait_time = 1.5
-	timer.autostart = true
+	# Add to the enemy group and print confirmation
+	add_to_group("Enemy")
+	print(name, " added to 'Enemy' group")
 	
-	add_child(timer)
-	# Connect the timer's timeout signal to our function
-	timer.timeout.connect(_on_timer_timeout)
-	
+	# Force trigger position update
 	
 
 func actor_setup():
@@ -34,44 +42,94 @@ func actor_setup():
 	await get_tree().physics_frame
 
 	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target(movement_target_position)
 
-func set_movement_target(movement_target: Vector3):
-	navigation_agent.set_target_position(movement_target)
+
+func get_npc_state():
+	return 'chase'
 
 func _physics_process(delta):
-	if navigation_agent.is_navigation_finished():
-		return
+	#if navigation_agent.is_navigation_finished():
+		#return
+	if get_npc_state() == 'chase':
+		navigation_agent.target_position = proto_controller.global_position
 
+	# navigate according to path
 	var current_agent_position: Vector3 = global_position
 	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
 
-	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+
+	#var diff = current_agent_position.direction_to(next_path_position) 
+	var diff = (next_path_position - current_agent_position)
 	
-	if velocity.length_squared() > 1.0:
+	# correct the height offset
+	diff = Vector3(diff.x, diff.y - height_offset_fix, diff.z)
+	var diff_normal = diff.normalized()
+	
+	
+	# path position (on ground) is always -18.4204998016357
+	# real position is -18.8272838592529
+	# offset is 0.406784058
+	#print(next_path_position.y, current_agent_position.y)
+	print(diff.y)
+	
+	#var flat_diff = Vector3(diff.x, 0, diff.z)
+	var want_velocity = diff_normal * movement_speed
+	velocity.x = want_velocity.x
+	velocity.z = want_velocity.z
+	
+	if not is_on_floor():
+		velocity.y += (get_gravity().y * delta)
+	
+	#print(diff.y)
+	if diff.y >= 0.1:
+		#pass
+		## need to jump
+		#$skeleton_mage2/AnimationPlayer.play("Idle")
+		print("Need to jump")
+		#if not is_on_floor():
+			##print("Is on air?")
+			##velocity.y = 0.0
+		##else:
+		print("Jumping")
+		$skeleton_mage2/AnimationPlayer.play("Jump_Full_Long")
+		velocity.y += jump_impulse
+	#el
+	if not is_on_floor():
+		$skeleton_mage2/AnimationPlayer.play("Jump_Full_Long")
+	elif velocity.length_squared() > 1.0:
 		$skeleton_mage2/AnimationPlayer.play("Walking_A")
 	else:
 		$skeleton_mage2/AnimationPlayer.play("Idle")
 	
+	
+	# Gravity
+	#if not is_on_floor():
+		#velocity += get_gravity()
 	#look_at(next_path_position)
  	# Create a temporary transform
 	var temp_transform = global_transform
 	
 	# Make it look at the target
-	temp_transform = temp_transform.looking_at(next_path_position, Vector3.UP)
-	
-	# Interpolate the current rotation to the desired rotation
-	global_transform.basis = global_transform.basis.slerp(temp_transform.basis, 0.25)
+	var flat_path_position: Vector3 = Vector3(next_path_position)
+	flat_path_position.y = current_agent_position.y
+	temp_transform = temp_transform.looking_at(flat_path_position, Vector3.UP)
+
+	global_transform.basis = global_transform.basis.slerp(temp_transform.basis, 0.1)
+
 	move_and_slide()
 
-func _on_timer_timeout():
-	# On poll event
-	print("Timeout occured")
-	var proto_controller = get_node("../ProtoController")
-		
-	if proto_controller:
-		# Call the set_movement_target function with the ProtoController's position
-		set_movement_target(proto_controller.global_position)
-	else:
-		push_error("ProtoController not found")
+
+#func _on_timer_timeout():
+	## On poll event
+	#print("Timeout occured")
+	#var proto_controller = get_node("../ProtoController")
+	#
+	#if not proto_controller:
+		#proto_controller = get_node("../../ProtoController")
+		#
+	#if proto_controller:
+		## Call the set_movement_target function with the ProtoController's position
+		#set_movement_target(proto_controller.global_position)
+	#else:
+		#push_error("ProtoController not found")
 	
